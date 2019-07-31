ab38b9143e59c09ec248eefb54cc66ea9207a7ba
John Geddeis <JohnGeddeis@outlook.com>
2019-07-31 13:00:30 -0400

    Dialog added to Story and Chance Classes

--- Chance.cs
+++ Chance.cs
@@ -9,7 +9,21 @@ namespace VoyagerProject
        
         public void ChanceToSpaceStation()
         {
-            Console.WriteLine();
+
+            Console.WriteLine("Hero watches as Colonel Donaldson and his men run towards the sound of battle.");
+            Console.WriteLine("Hero thinks- “I doubt the Colonel will mind if I borrow the Hermes”");
+            Console.WriteLine(" * enters the rear hatch and moves quickly to the cockpit.");
+            Console.WriteLine(" Shaking from adrenaline the hero tries to get the ship off the ground, but is it the red button or green switch? ");
+            //artificial choice both buttons will strat the craft.
+            Console.WriteLine("* The ship begins hovering.");
+            Console.WriteLine(" Hero thinks- “At least my Conscription if finally paying off. ”");
+            Console.WriteLine(" The hero points the ship to the sky and launches. ");
+            Console.WriteLine(".....");
+            Console.WriteLine(" A familiar voice comes over the radio ");
+            Console.WriteLine(" After traveling for an hour the radio begins to crackle with life.");
+            Console.WriteLine("Donaldson- “You son of * fizz fizzzzz * I tell you how to save yourself and you steal *fizz fizzzzz* ship to thank me?! ”");
+
+
         }
         public void ChanceToProxima()
         {
