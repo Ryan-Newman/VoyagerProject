cb79c765cef9d1d0ffdb4dcd9cbed9833a64f63d
Ryan Newman <Ryan.N.Newman@Outlook.com>
2019-08-01 09:39:07 -0400

    Added a broken button to Choices class, almost ther but not quite.

--- Choices.cs
+++ Choices.cs
@@ -17,14 +17,16 @@ namespace VoyagerProject
 
         public void ChoiceSpaceShip()
         {
-            Console.WriteLine("What Choice will you make?");// The user will make a choice left or right // Later this project will have a selector button
-            string choice = Console.ReadLine();
-            if (choice == "left")
+           // Console.WriteLine("What Choice will you make?");// The user will make a choice left or right // Later this project will have a selector button
+            string stealShip = "Steal The Ship!";
+            string takeCover = "Take Cover";
+            Button(stealShip, takeCover);
+            if (stealShip == "chance")
             {
                 Safe safe = new Safe();
                 safe.SafeToSpaceStation(); // Goes to Safe class and calls the dialogue for the safe adventure
             }
-            else if (choice == "right")
+            else if (takeCover == "safe")
             {
                 Chance chance = new Chance();
                 //dice.RngDice(choice);
@@ -149,20 +151,150 @@ namespace VoyagerProject
                 //dice.RngDice(choice);
                 chance.ChanceToEarth();
             }
-        }
+        }       
 
-        public string Chance(string chance)//Chance Path
+        public void Button(string chanceButton, string safeButton)
         {
+            int index = 0;
 
-            return chance;
-        }
+            string[] menu = new string[] { chanceButton, safeButton };
 
-        public string Safe(string safe)//Safe Path
-        {
+            ConsoleKeyInfo ckey;
 
-            return safe;
+            Console.CursorVisible = false;
+            do
+            {
+                for (int i = 0; i < menu.Length; i++)
+                {
+                    if (i == index)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine(menu[i]);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine(menu[i]);
+                    }
+                    Console.ResetColor();
+
+                }
+
+                ckey = Console.ReadKey();
+
+                if (ckey.Key == ConsoleKey.DownArrow)
+                {
+                    if (index == menu.Length - 1)
+                    {
+                        index = 0;
+                    }
+                }
+                else
+                {
+                    index++;
+                }
+
+                if (ckey.Key == ConsoleKey.UpArrow)
+                {
+                    if (index <= 0)
+                    {
+                        index = menu.Length - 1;
+                    }
+                }
+                
+                if(ckey.Key == ConsoleKey.Enter && index == 0)
+                    {
+                        Safe safe = new Safe();
+                        safe.SafeToSpaceStation(); // Goes to Safe class and calls the dialogue for the safe adventure
+                    }
+                else if(ckey.Key == ConsoleKey.Enter && index > 0)
+                    {
+                        Chance chance = new Chance();
+                        //dice.RngDice(choice);
+                        chance.ChanceToSpaceStation();
+                    }
+                    else
+                    { index--; }
+
+                Console.Clear(); // !!!Bug!!! Erases the former text line
+            } while (ckey.Key != ConsoleKey.Escape);
         }
 
 
     }
 }
+            //Console.Clear();
+            //Boolean running = true;
+            //Console.CursorVisible = false;
+            //int select = 0;
+            //List<Tuple<int, int, string, Boolean>> options = new List<Tuple<int, int, string, Boolean>>
+            //{
+            //    new Tuple<int,int,string,Boolean>(0,0,chanceButton,true),
+            //    new Tuple<int,int,string,Boolean>(0,1,safeButton,false),
+            //};
+            //while (running == true)
+            //{
+            //    foreach(Tuple<int,int,string,Boolean> tupe in options)
+            //    {
+            //        if (tupe.Item4 == true)
+            //        {
+            //            select = options.FindIndex(t => tupe.Item3 == tupe.Item3);
+            //            Console.SetCursorPosition(tupe.Item1, tupe.Item2);
+            //            Console.ForegroundColor = ConsoleColor.Black;
+            //            Console.BackgroundColor = ConsoleColor.White;
+            //            Console.Write(tupe.Item3);
+
+            //        }
+            //        else if(tupe.Item4 == false)
+            //        {
+            //            Console.SetCursorPosition(tupe.Item1, tupe.Item2);
+            //            Console.ForegroundColor = ConsoleColor.White;
+            //            Console.BackgroundColor = ConsoleColor.Black;
+            //            Console.Write(tupe.Item3);
+            //        }
+            //    }
+            //    Console.SetCursorPosition(options[0].Item1, options[0].Item2);
+            //    Console.ForegroundColor = ConsoleColor.White;
+            //    Console.BackgroundColor = ConsoleColor.Black;
+            //    Console.Write("");
+
+            //    string input = Console.ReadKey().Key.ToString();
+            //    if (input == "LeftArrow" && select > 0)
+            //    {
+            //        options[select] = new Tuple<int, int, string, bool>(options[select].Item1, options[select].Item2, options[select].Item3, false);
+            //        select -= 1;
+            //        options[select] = new Tuple<int, int, string, bool>(options[select].Item1, options[select].Item2, options[select].Item3, true);
+            //    }
+            //    else if (input == "LeftArrow" && select == 0)
+            //    {
+            //        options[select] = new Tuple<int, int, string, bool>(options[select].Item1, options[select].Item2, options[select].Item3, false);
+            //        select += 1;
+            //        options[select] = new Tuple<int, int, string, bool>(options[select].Item1, options[select].Item2, options[select].Item3, true);
+
+            //    }
+            //    if (input == "RightArrow" && select < 1)
+            //    {
+            //        options[select] = new Tuple<int, int, string, bool>(options[select].Item1, options[select].Item2, options[select].Item3, false);
+            //        select += 1;
+            //        options[select] = new Tuple<int, int, string, bool>(options[select].Item1, options[select].Item2, options[select].Item3, true);
+            //    }
+            //    else if (input == "RightArrow" && select == 1)
+            //    {
+            //        options[select] = new Tuple<int, int, string, bool>(options[select].Item1, options[select].Item2, options[select].Item3, false);
+            //        select -= 1;
+            //        options[select] = new Tuple<int, int, string, bool>(options[select].Item1, options[select].Item2, options[select].Item3, true);
+
+            //    }
+            //    if (input == "Enter" && select == 0)
+            //    {
+            //        chanceButton = "CHANCE";
+                   
+                    
+            //    }
+            //    if (input == "Enter" && select == 1)
+            //    {
+            //        safeButton = "SAFE";
+                   
+            //    }
